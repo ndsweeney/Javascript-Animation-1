28b481a8a3c0402d3ab7128fc9ba02bcd472bd01
Nathan Sweeney <ndsweeney@gmail.com>
2013-04-03 17:59:57 +0100

    Upload
    Main upload

---
+++ JQ2/js/js_lib.js
@@ -0,0 +1,182 @@
+//Red line functions
+
+function line_1_red()
+{
+
+	$('#line_1_red').addClass('grow');
+	setTimeout('line_2_red()', 1000);	
+
+}
+function line_2_red()
+{
+
+	$('#line_2_red').addClass('grows');
+	setTimeout('line_3_red()', 1000);	
+
+}
+
+function line_3_red()
+{
+
+	$('#line_3_red').addClass('growr1');
+	$('#line_4_red').addClass('growr2');
+	setTimeout('line_5_red();', 1000);		
+
+}
+
+function line_5_red()
+{
+
+	$('#line_5_red').addClass('growr3');
+	$('#line_6_red').addClass('growr4');
+	setTimeout('line_6_red();', 1000);		
+
+}
+
+function line_6_red()
+{
+
+	$('#line_7_red').addClass('growr5');
+	$('#line_8_red').addClass('growr6');
+	setTimeout('line_9_red();', 1000);		
+
+}
+
+function line_9_red()
+{
+
+	$('#line_9_red').addClass('growr7');
+	setTimeout('line_1_blue();', 1000);		
+
+}
+
+
+//Blue line functions
+
+function line_1_blue()
+{
+
+	$('#line_1_blue').addClass('growb1');
+	setTimeout('line_2_blue()', 1000);
+
+}
+
+function line_2_blue()
+{
+
+	$('#line_2_blue').addClass('growb2');
+	setTimeout('line_3_blue()', 1000);
+
+}
+
+function line_3_blue()
+{
+
+	$('#line_3_blue').addClass('growb3');
+	setTimeout('line_4_blue()', 500);
+	
+}
+
+function line_4_blue()
+{
+
+	$('#line_4_blue').addClass('growb4');
+	setTimeout('line_5_blue()', 500);
+
+}
+
+function line_5_blue()
+{
+
+	$('#line_5_blue').addClass('growb5');
+	setTimeout('line_6_blue()', 500);
+
+}
+
+function line_6_blue()
+{
+
+	$('#line_6_blue').addClass('growb6');
+	setTimeout('line_7_blue()', 500);
+
+}
+
+function line_7_blue()
+{
+
+	$('#line_7_blue').addClass('growb7');
+	setTimeout('line_8_blue()', 500);
+
+}
+
+function line_8_blue()
+{
+
+	$('#line_8_blue').addClass('growb8');
+	setTimeout('line_9_blue()', 500);
+
+	//Would like to start red here 
+	
+}
+
+function line_9_blue()
+{
+
+	$('#line_9_blue').addClass('growb9');
+	setTimeout('line_10_blue()', 1250);
+
+}
+
+function line_10_blue()
+{
+
+	$('#line_10_blue').addClass('growb10');
+	setTimeout('line_11_blue()', 1000);
+
+}
+
+function line_11_blue()
+{
+
+	$('#line_11_blue').addClass('growb11');
+	$('#line_12_blue').addClass('growb12');
+
+	setTimeout('line_13_blue()', 1000);
+
+}
+
+function line_13_blue()
+{
+
+	$('#line_13_blue').addClass('growb13');
+	$('#line_14_blue').addClass('growb14');
+
+	setTimeout('line_15_blue()', 1000);
+
+}
+
+function line_15_blue()
+{
+
+	$('#line_15_blue').addClass('growb15');
+	$('#line_16_blue').addClass('growb16');
+
+	setTimeout('line_17_blue()', 1000);
+
+}
+
+function line_17_blue()
+{
+
+	$('#line_17_blue').addClass('growb17');
+
+	setTimeout('line_18_green()', 1000);
+
+}
+
+function line_18_green()
+{
+
+	$('#l1bt').addClass('growg1');
+
+}
